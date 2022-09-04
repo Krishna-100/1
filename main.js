fab0a9735197fc5d2c79c3fb92ac4cc3392f3e0f
Krishna-100 <107849496+Krishna-100@users.noreply.github.com>
2022-09-04 16:32:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function trans()
+{
+    window.location = "bedroom.html";
+}
+function trans1()
+{
+    window.location = "TV and Ac.html";
+}
+function trans2()
+{
+    window.location = "Desk.html";
+}
+function trans3()
+{
+    window.location = "Bottles.html";
+}
+function trans4()
+{
+    window.location = "fruit basket.html";
+}
+function back1()
+{
+    window.location = "index.html";
+}
+function back2()
+{
+    window.location = "index.html";
+}
+function back3()
+{
+    window.location = "index.html";
+}
+
+function back()
+{
+    window.location = "index.html";
+}
+
+
